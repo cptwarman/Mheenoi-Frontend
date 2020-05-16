f2e248e28159e3cb88cb7e5fc4f410165ef71f06
cptwarman <48947748+cptwarman@users.noreply.github.com>
2020-05-16 18:35:28 +0700

    fix info student

--- src/views/student/student_info.vue
+++ src/views/student/student_info.vue
@@ -621,7 +621,7 @@ export default {
      let jwtToken = sessionStorage.getItem('jwt')
      axios({
           method: 'put',
-          url: `https://chai-test-backend.herokuapp.com/api/students/${this.$store.getters.getStudentId}/info`,
+          url: `https://chai-test-backend.herokuapp.com/api/students/${this.$store.getters.getId}/info`,
           data: { 
            payload: this.passPayload },
          headers: {
